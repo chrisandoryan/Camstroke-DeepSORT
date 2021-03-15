separate font calculation between proportional and fixed-width font type

text_processing/cca.py
@@ -3,7 +3,12 @@ from PIL import Image, ImageOps
 import numpy as np
 
 """
-Script that runs connected component labelling for isolating characters from image
+Performs connected component labelling (CCA/CCL) for isolating characters from image.
+Algorithm:
+1. perform CCA and isolate character regions
+2. eliminate regions that are cut off (the bbox's x or y is intersected with the border of the image)
+3. 
+
 """
 
 CONNECTIVITY = 8
@@ -24,22 +29,35 @@ def isolate_components(im, output):
         area = stats[i, cv2.CC_STAT_AREA]
         (bbox_cX, bbox_cY) = centroids[i]
 
-        # clone our original image (so we can draw on it) and then draw
-        # a bounding box surrounding the connected component along with
-        # a circle corresponding to the centroid
-        cloned_frame = im.copy()
-        cv2.rectangle(cloned_frame, (bbox_x, bbox_y), (bbox_x + bbox_w, bbox_y + bbox_h), (0, 255, 0), 3)
-        cv2.circle(cloned_frame, (int(bbox_cX), int(bbox_cY)), 4, (0, 0, 255), -1)
+        print("W", bbox_w)
+        print("H", bbox_h)
+        print("Area", area)
 
-        # construct a mask for the current connected component by
-        # finding a pixels in the labels array that have the current
-        # connected component ID
-        componentMask = (labels == i).astype("uint8") * 255
-        
-        # show our output image and connected component mask
-        cv2.imshow("Output", cloned_frame)
-        # cv2.imshow("Connected Component", componentMask)
-        cv2.waitKey(0)
+        # ensure the width, height, and area are all neither too small
+        # nor too big
+        keepWidth = True # bbox_w > 5 and bbox_w < 50
+        keepHeight = True # bbox_h > 45 and bbox_h < 65
+        keepArea = True # area > 500 and area < 1500
+
+        # ensure the connected component we are examining passes all
+        # three tests
+        if all((keepWidth, keepHeight, keepArea)):
+            # clone our original image (so we can draw on it) and then draw
+            # a bounding box surrounding the connected component along with
+            # a circle corresponding to the centroid
+            cloned_frame = im.copy()
+            cv2.rectangle(cloned_frame, (bbox_x, bbox_y), (bbox_x + bbox_w, bbox_y + bbox_h), (0, 255, 0), 3)
+            cv2.circle(cloned_frame, (int(bbox_cX), int(bbox_cY)), 4, (0, 0, 255), -1)
+
+            # construct a mask for the current connected component by
+            # finding a pixels in the labels array that have the current
+            # connected component ID
+            componentMask = (labels == i).astype("uint8") * 255
+
+            # show our output image and connected component mask
+            cv2.imshow("Output", cloned_frame)
+            cv2.imshow("Connected Component", componentMask)
+            cv2.waitKey(0)
 
 def run_with_stats(im):
     im = np.array(im)